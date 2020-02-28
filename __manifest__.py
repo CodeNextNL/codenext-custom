d8c7b8bb8bce9647bb6538cf12b88c39dbcd8dac
Wytze Jan Riedstra <wjriedstra@gmail.com>
2020-02-28 12:39:42 +0100

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "codeNext Custom CSS",
+    'summary': 'codeNext Custom CSS',
+    'author': "codeNext",
+    'website': "http:/www.codenext.nl",
+    'version': '11.0.0.1',
+    'installable': True,
+    'license': 'LGPL-3',
+    'data': [
+        'views/codenext_custom_css_templates.xml',
+    ]
+}
